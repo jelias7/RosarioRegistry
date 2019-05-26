dbca21a3bd5d6f3a0a8375b084fdecb26dcfa1f8
Juan Rosario <juanelias18@hotmail.com>
2019-05-25 20:34:50 -0400

    Consulta de Usuarios completada

---
+++ UI/Consultas/cUsuarios.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Rosario_Registry.UI.Consultas
+{
+    partial class cUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CriteriotextBox = new System.Windows.Forms.TextBox();
+            this.FiltrarcomboBox = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.DesdedateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Consultabutton = new System.Windows.Forms.Button();
+            this.ConsultadataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Filtro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Criterio";
+            // 
+            // CriteriotextBox
+            // 
+            this.CriteriotextBox.Location = new System.Drawing.Point(347, 45);
+            this.CriteriotextBox.Name = "CriteriotextBox";
+            this.CriteriotextBox.Size = new System.Drawing.Size(205, 20);
+            this.CriteriotextBox.TabIndex = 4;
+            // 
+            // FiltrarcomboBox
+            // 
+            this.FiltrarcomboBox.FormattingEnabled = true;
+            this.FiltrarcomboBox.Items.AddRange(new object[] {
+            "Todo",
+            "ID",
+            "Nombres",
+            "Email",
+            "Nivel Usuario",
+            "Usuario",
+            "Clave"});
+            this.FiltrarcomboBox.Location = new System.Drawing.Point(218, 44);
+            this.FiltrarcomboBox.Name = "FiltrarcomboBox";
+            this.FiltrarcomboBox.Size = new System.Drawing.Size(105, 21);
+            this.FiltrarcomboBox.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(118, 45);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(80, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // DesdedateTimePicker
+            // 
+            this.DesdedateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.DesdedateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DesdedateTimePicker.Location = new System.Drawing.Point(16, 44);
+            this.DesdedateTimePicker.Name = "DesdedateTimePicker";
+            this.DesdedateTimePicker.Size = new System.Drawing.Size(84, 20);
+            this.DesdedateTimePicker.TabIndex = 7;
+            // 
+            // Consultabutton
+            // 
+            this.Consultabutton.Image = global::Rosario_Registry.Properties.Resources.Search_find_locate_1542;
+            this.Consultabutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Consultabutton.Location = new System.Drawing.Point(573, 42);
+            this.Consultabutton.Name = "Consultabutton";
+            this.Consultabutton.Size = new System.Drawing.Size(84, 24);
+            this.Consultabutton.TabIndex = 8;
+            this.Consultabutton.Text = "Consultar";
+            this.Consultabutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Consultabutton.UseVisualStyleBackColor = true;
+            this.Consultabutton.Click += new System.EventHandler(this.Consultabutton_Click);
+            // 
+            // ConsultadataGridView
+            // 
+            this.ConsultadataGridView.AllowUserToAddRows = false;
+            this.ConsultadataGridView.AllowUserToDeleteRows = false;
+            this.ConsultadataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConsultadataGridView.Location = new System.Drawing.Point(13, 80);
+            this.ConsultadataGridView.Name = "ConsultadataGridView";
+            this.ConsultadataGridView.ReadOnly = true;
+            this.ConsultadataGridView.Size = new System.Drawing.Size(644, 298);
+            this.ConsultadataGridView.TabIndex = 9;
+            // 
+            // cUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 390);
+            this.Controls.Add(this.ConsultadataGridView);
+            this.Controls.Add(this.Consultabutton);
+            this.Controls.Add(this.DesdedateTimePicker);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.FiltrarcomboBox);
+            this.Controls.Add(this.CriteriotextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "cUsuarios";
+            this.Text = "Consulta Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox CriteriotextBox;
+        private System.Windows.Forms.ComboBox FiltrarcomboBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker DesdedateTimePicker;
+        private System.Windows.Forms.Button Consultabutton;
+        private System.Windows.Forms.DataGridView ConsultadataGridView;
+    }
+}
---
+++ UI/Consultas/cUsuarios.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rosario_Registry.Entidades;
+using Rosario_Registry.BLL;
+
+namespace Rosario_Registry.UI.Consultas
+{
+    public partial class cUsuarios : Form
+    {
+        public cUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Consultabutton_Click(object sender, EventArgs e)
+        {
+            var listado = new List<Usuarios>();
+
+            if (CriteriotextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltrarcomboBox.Text)
+                {
+                    case "Todo":
+                        listado = UsuariosBLL.GetList(p => true);
+                        break;
+
+                    case "ID":
+                        int id = Convert.ToInt32(CriteriotextBox.Text);
+                        listado = UsuariosBLL.GetList(p => p.UsuarioID == id);
+                        break;
+
+                    case "Nombres":
+                        listado = UsuariosBLL.GetList(p => p.Nombres.Contains(CriteriotextBox.Text));
+                        break;
+
+                    case "Email":
+                        listado = UsuariosBLL.GetList(p => p.Email.Contains(CriteriotextBox.Text));
+                        break;
+
+                    case "Nivel Usuario":
+                        listado = UsuariosBLL.GetList(p => p.NivelUsuario.Contains(CriteriotextBox.Text));
+                        break;
+
+                    case "Usuario":
+                        listado = UsuariosBLL.GetList(p => p.Usuario.Contains(CriteriotextBox.Text));
+                        break;
+
+                    case "Clave":
+                        int c = Convert.ToInt32(CriteriotextBox.Text);
+                        listado = UsuariosBLL.GetList(p => p.Clave == c);
+                        break;
+
+                    default:
+                        break;
+                }
+                listado = listado.Where(c => c.FechaIngreso.Date >= DesdedateTimePicker.Value.Date && c.FechaIngreso.Date <= HastadateTimePicker.Value.Date).ToList();
+            }
+            else
+            {
+                listado = UsuariosBLL.GetList(p => true);
+            }
+
+            ConsultadataGridView.DataSource = null;
+            ConsultadataGridView.DataSource = listado;
+        }
+    }
+}
